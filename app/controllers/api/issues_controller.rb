d6f50400202d241aa5c09975c6ab2c1b178847cd
Jimmy Wu <jimmy2822@gmail.com>
2022-09-18 22:29:23 +0800

    * Implement api/issues/:id/agree * Implement api/issues/:id/disagree

--- app/controllers/api/issues_controller.rb
+++ app/controllers/api/issues_controller.rb
@@ -2,6 +2,8 @@
 
 module Api
   class IssuesController < ApplicationController
+    before_action :find_issue, only: [:agree, :disagree]
+
     def index
       issues = Issue.page(params[:page]).per(params[:per_page] || 10)
 
@@ -21,15 +23,31 @@ module Api
     end
 
     def agree
+      redis.set("issue_#{params[:id]}_#{source_request_ip}", 'agree')
+
+      render_success_response(data: { voted_result: 'agree' })
     end
 
     def disagree
+      redis.set("issue_#{params[:id]}_#{source_request_ip}", 'disagree')
+
+      render_success_response(data: { voted_result: 'disagree' })
     end
 
     private
 
+    def find_issue
+      @issue = Issue.find(params[:id])
+    rescue
+      render_error_response(code: 404, message: 'issue not found')
+    end
+
     def issue_params
       params.permit(:name)
+    end
+
+    def redis
+      @redis ||= Redis.new
     end
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,6 @@ class ApplicationController < ActionController::API
   def check_request_rate_limit
     redis = Redis.new
 
-    source_request_ip = request.env['REMOTE_ADDR']
     source_ip_counter = redis.incr(source_request_ip)
     redis.expire(source_request_ip, 60) if source_ip_counter == 1
 
@@ -24,5 +23,11 @@ class ApplicationController < ActionController::API
 
   def render_success_response(data:, code: 200, message: 'success')
     render json: { status_code: code, data: data, message: message }
+  end
+
+  private
+
+  def source_request_ip
+    request.env['REMOTE_ADDR']
   end
 end
